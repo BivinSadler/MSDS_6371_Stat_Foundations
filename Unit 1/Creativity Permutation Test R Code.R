9745094689c1fbf0c859acb9d668eda153cc4fc0
BivinSadler <43288506+BivinSadler@users.noreply.github.com>
2020-05-07 22:50:44 -0500

    Add files via upload

--- Unit 1/Creativity Permutation Test R Code.R
+++ Unit 1/Creativity Permutation Test R Code.R
@@ -24,24 +24,26 @@ counter = 0;
 
 for(i in 1:number_of_permutations)
 {
-  scramble = sample(create$Score,47); s#huffle the observations
+  scramble = sample(create$Score,47); #shuffle the observations
   # the below two lines effectively radnomly applies the extrinsic and intrinsic labels to the observations. 
   extrinsic = scramble[1:23]; # give the first 23 the extrinsic label
   intrinsic = scramble[24:47]; #give the rest the intrinsic lable. 
   diff = mean(intrinsic)-mean(extrinsic); # find the difference in sample means from current permutation. under the equality assumtion, most of the time this should be close to zero
   xbarholder[i] = diff; # remember the differene in sample means to compare to the one we observed in the study... 
   
-  \#if the one we observed sticks out then it will be hard to for the diffs to exceed it and that will be evidence that is doesn't 
+  #if the one we observed sticks out then it will be hard to for the diffs to exceed it and that will be evidence that is doesn't 
   #belong and thus is evidence that the difference is not zero and thus that the means are not equal and thus that there is a 
   #treatment effect. 
   
-  evidence that the means are not equal 
-  if(diff > observed_diff) 
+  #evidence that the means are not equal 
+  if(abs(diff) > observed_diff) 
     counter = counter + 1;
   
 }
 hist(xbarholder);
-pvalue = counter / number_of_permutations; # the pvalue is the percentage of the differences in sample means that were generaged under 
+pvalue = counter / number_of_permutations; 
+pvalue
+# the pvalue is the percentage of the differences in sample means that were generaged under 
 #the assumption of equal means exceed the one we observed.  If this percentage is small, then this is evidence that the observed 
 #difference does not belong in this distribution and thus that the difference is not zero and thus that the means are not equal and thus that there is a 
 #treatment effect. 
