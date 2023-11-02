825718cda35876a0381c87ed866a1cadf08f563b
BivinSadler <43288506+BivinSadler@users.noreply.github.com>
2023-11-02 00:01:45 -0500

    Add files via upload

---
+++ Unit 11/InfantVGDPDataGather.R
@@ -0,0 +1,37 @@
+# Note: The code below depends on the World Bank keeping the format with which they presen the data.
+# If you run into an error, please first try and print out the results along the way to see 
+# if you can figure out how to fix it.  If after 30 min to an hour it is still a problem, feel free to reach
+# out and I will find a solution.  
+
+#install.packages("WDI")
+library(tidyverse)
+library(WDI)
+WDIsearch("infant") # yields the indicator: SP.DYN.IMRT.IN
+WDIsearch("GDP") # yields the indicator: NY.GDP.PCAP.CD
+InfantMort = WDI(,"SP.DYN.IMRT.IN",start = 2019, end = 2019)
+GDPs = WDI(,"NY.GDP.PCAP.CD",start = 2019, end = 2019)
+InfantVGDP = inner_join(GDPs,InfantMort, "country")
+InfantVGDP = InfantVGDP %>% select("country","NY.GDP.PCAP.CD","SP.DYN.IMRT.IN")
+colnames(InfantVGDP) = c("country","GDP","InfantMort")
+InfantVGDP
+
+
+# Plot on Raw data
+InfantVGDP %>% ggplot(aes(x = GDP, y = InfantMort)) + geom_point() + ggtitle("Infant Mortality versus GDP")
+#Plot on linear-log
+InfantVGDP %>% ggplot(aes(x = GDP, y = log(InfantMort))) + geom_point() + ggtitle("log(Infant Mortality) versus GDP")
+#Plot on log-linear
+InfantVGDP %>% ggplot(aes(x = log(GDP), y = InfantMort)) + geom_point() + ggtitle("Infant Mortality versus log(GDP)")
+#Plot on log-log
+InfantVGDP %>% ggplot(aes(x = log(GDP), y = log(InfantMort))) + geom_point() + ggtitle("log(Infant Mortality) versus log(GDP)")
+
+#Correlation
+library(corr)
+cor.test(InfantVGDP$InfantMort,InfantVGDP$GDP)
+
+
+fitloglog = lm(log(InfantMort)~log(GDP), data = InfantVGDP)
+
+fitlinlog = lm(InfantMort~log(GDP), data = InfantVGDP)
+
+
